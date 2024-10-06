b6e984698c85ab22c286f6646d8ff6e8508bfe01
pmensah28 <princemensaheyram@gmail.com>
2024-10-06 16:13:34 +0000

    Add setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sent_analysis_project",
+    version="0.0.0",
+    author="Username",
+    author_email="email",
+    packages=find_packages()
+)
