3f170b5ed23690880fb7f31bd31a10f56b2312fb
pmensah28 <princemensaheyram@gmail.com>
2024-10-16 00:29:32 +0000

    Add models to gitignore

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="sent_analysis_project",
-    version="0.0.0",
-    author="Username",
-    author_email="email",
-    packages=find_packages()
-)
